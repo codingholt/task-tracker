db1ad345f1e3a1458d1e4b647bfb4b5ca0d45527
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-04 20:16:33 +0100

    added styling

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+  return (
+    <footer>
+        App made by Sven
+        <div className='links'>
+        <a href='https://github.com/codingholt'>Github</a>
+        <a href='https://twitter.com/traderholt'>Twitter</a>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer
