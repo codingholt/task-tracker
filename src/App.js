2a819e07f9e6cca51739f946273ab2276f377c4a
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-04 23:09:44 +0100

    added more components and a useless btn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Tasks from './components/Tasks';
 
 function App() {
   return (
     <div className="container">
       <Header/>
+      
       <Footer/>
+      
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './header.css'
-
+import Button from './Button';
 const Header = () => {
   return (
     <header>
         <h1>TaskTracker</h1>
+        <Button color='black' text='Add'/>
     </header>
   );
 }
+
+
 
 export default Header
