63833d0de23eb88d60fb3e906e7591d483904ae2
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-06 11:07:42 +0100

    form working now, adding new task

--- src/App.js
+++ src/App.js
@@ -33,6 +33,13 @@ function App() {
       
       ])
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask =  {id, ...task}
+
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id) =>{
     setTasks(tasks.filter((task) =>  task.id !== id))
   }
@@ -46,7 +53,7 @@ function App() {
   return (
     <div className="container">
       <Header/>
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ?
       ( <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> )
       : ('no tasks...')}
