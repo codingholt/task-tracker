5c3245c63cf7aec9ccb6609de33880bf84d2e77d
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2022-03-09 16:17:02 +0100

    moved to sqllite db and local db file

--- server/server.js
+++ server/server.js
@@ -3,19 +3,24 @@ const cors = require('cors')
 
 const PORT = 3001
 
-const mysql = require('mysql2')
+const sqlite3 = require('sqlite3')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const db = mysql.createConnection({ //Create connection to the MySQL database
-    user: 'root',
-    host: 'localhost',
-    password: 'MySql0510',
-    database: 'taskTracker'
-})
+let db = new sqlite3.Database('./db/tasks.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the tasks database.');
+  });
+
+
+const sql = 'CREATE TABLE tasks(id int,task text(255),day text(255),reminder boolean,);'
+
+
 
 app.post('/new', (req, res) => {
     const id = req.body.id;
@@ -23,7 +28,7 @@ app.post('/new', (req, res) => {
     const day = req.body.day;
     const reminder = req.body.reminder;
 
-    db.query('INSERT INTO tasks (id, task, day, reminder) VALUES (?,?,?,?)', 
+    db.run('INSERT INTO tasks (id, task, day, reminder) VALUES (?,?,?,?)', 
     [id, task, day, reminder],
         (err, result) =>{
             if(err){
